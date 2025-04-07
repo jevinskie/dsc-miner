46090d3c5559f7d30eac910c1f82912e5e45e62a
Jevin Sweval <jevinsweval@gmail.com>
2025-04-07 15:34:51 -0400

    Refactor BitVec to use std::vector for memory management

--- include/dsc-miner/bitvec.hpp
+++ include/dsc-miner/bitvec.hpp
@@ -6,24 +6,16 @@
 #include <bit>
 #include <cstddef>
 #include <cstdint>
-#include <cstdlib>
+#include <vector>
 
 using elem_t = uint64_t;
 
 class BitVec {
 public:
-    BitVec(size_t nbits) : m_nbits{nbits} {
-        if (nbits % (sizeof(elem_t) * 8)) {
+    BitVec(size_t nbits) : m_nbits{nbits}, m_buf(size_elem()) {
+        if (nbits % size_one_elem_bits()) {
             abort();
         }
-        buf = (elem_t *)calloc(1, size_bytes());
-        if (!buf) {
-            abort();
-        }
-    }
-
-    ~BitVec() {
-        free(buf);
     }
 
     size_t size() const {
@@ -34,31 +26,39 @@ public:
         return m_nbits / 8;
     }
 
+    size_t size_one_elem() const {
+        return sizeof(elem_t);
+    }
+
+    size_t size_one_elem_bits() const {
+        return size_one_elem() * 8;
+    }
+
     size_t size_elem() const {
-        return size_bytes() / sizeof(elem_t);
+        return size_bytes() / size_one_elem();
     }
 
     size_t elem_idx(size_t bit_idx) const {
-        return bit_idx / (sizeof(elem_t) * 8);
+        return bit_idx / size_one_elem_bits();
     }
 
     size_t elem_off(size_t bit_idx) const {
-        return bit_idx % (sizeof(elem_t) * 8);
+        return bit_idx % size_one_elem_bits();
     }
 
     void set(size_t bit_idx) {
-        buf[elem_idx(bit_idx)] |= 1ull << elem_off(bit_idx);
+        m_buf[elem_idx(bit_idx)] |= 1ull << elem_off(bit_idx);
     }
 
     uint64_t popcnt() const {
         uint64_t cnt = 0;
         for (size_t i = 0; i < size_elem(); ++i) {
-            cnt += std::popcount(buf[i]);
+            cnt += std::popcount(m_buf[i]);
         }
         return cnt;
     }
 
 private:
     const size_t m_nbits;
-    elem_t *buf;
+    std::vector<elem_t> m_buf;
 };
